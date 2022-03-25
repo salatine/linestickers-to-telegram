ef5b5b0118317e1f7de88c61e6fb673e29aa48e3
salatinee <sawurora@gmail.com>
2022-03-25 19:32:51 -0300

    .

--- import_telegram_stickers/variables.py
+++ import_telegram_stickers/variables.py
@@ -1,14 +1,14 @@
 # your bot's username, like 'mybotisverycool'
-TELEGRAM_BOT_USERNAME = 'importstickersbot'
+TELEGRAM_BOT_USERNAME = None
 
 # your bot's token, like '110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw'
-TELEGRAM_BOT_TOKEN = '5273339526:AAFMla6CX3LyX8-02orD8mCUq8JZqYma2uw'
+TELEGRAM_BOT_TOKEN = None
 
 # your User ID, like 3840182041
-USER_ID = 1081415869
+USER_ID = None
 
 # a name for your sticker pack, like 'mycoolstickerpack'
-STICKER_NAME = 'testverycool'
+STICKER_NAME = None
 
 # a title for your sticker pack, like 'My Cool Sticker Pack'
-STICKER_TITLE = 'test very cool'
+STICKER_TITLE = None
